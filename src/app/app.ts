0e0089b237cdb0e83b8b6066697160712c3de825
Adria <adria.jorda.m@gmail.com>
2025-08-07 09:10:01 +0200

    [add] codigo html nav y base de los componentes en main añadidos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './nav/nav'
+import { Aboutme } from './aboutme/aboutme'
+import { Landing } from './landing/landing'
+import { Contact } from './contact/contact'
+import { Skills } from './skills/skills'
+import { Portfolio } from './portfolio/portfolio'
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet, Nav, Landing, Aboutme, Skills, Portfolio, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

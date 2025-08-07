0cc60a10d61a2afd6da50c5bee1498873f4c1282
Adria <adria.jorda.m@gmail.com>
2025-08-07 08:45:40 +0200

    [add] reseteado a estado inicial, por que vaya follon habia

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Nav } from './nav/nav';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
